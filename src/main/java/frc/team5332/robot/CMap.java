edd25534632c0e346bc88ca930f1b7abd58999e4
Jonathan Daniel <gcagrad19@gmail.com>
2019-01-06 16:03:59 -0500

    Added Gamepad to CMap for #1
    Port number is currently 0

--- src/main/java/frc/team5332/robot/CMap.java
+++ src/main/java/frc/team5332/robot/CMap.java
@@ -1,6 +1,11 @@
 package frc.team5332.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class CMap {
+    // Joysticks
+    Joystick gamepad = new Joystick(0);
+
     // Motors
 
     // Encoders
