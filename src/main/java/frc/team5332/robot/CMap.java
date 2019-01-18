e0da954e17ca7c5e294fb8de62276f40cdb0f155
Jonathan Daniel <gcagrad19@gmail.com>
2019-01-18 03:06:14 -0500

    Added First Half of Vision Equations
    Added the one where assuming we have the angle, we convert it to a encoder setpoint. Need to figure out getting that angle.

--- src/main/java/frc/team5332/robot/CMap.java
+++ src/main/java/frc/team5332/robot/CMap.java
@@ -3,6 +3,7 @@ package frc.team5332.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.team5332.commands.HatchGrabber.ChangeGrabberState;
+import frc.team5332.subsystems.Drivetrain;
 import frc.team5332.subsystems.HatchGrabber;
 
 public class CMap {
@@ -16,8 +17,11 @@ public class CMap {
     // Pneumatics
 
     // Subsystems
- public static HatchGrabber hatchGrabber = new HatchGrabber();
- public static JoystickButton greenbutton = new JoystickButton(gamepad,1);
+    public static HatchGrabber hatchGrabber = new HatchGrabber();
+    public static Drivetrain drivetrain = new Drivetrain();
+
+    public static JoystickButton greenbutton = new JoystickButton(gamepad,1);
+
     public static void setupJoystickButtons(){
                 greenbutton.whenPressed(new ChangeGrabberState());
                 System.out.println("setupjoystick");
