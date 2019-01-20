6a707d3e54896df071c6ef6359108bfdc4874b25
isabella <isabellalunasol12@gmail.com>
2019-01-20 15:34:07 -0500

    Started TimeDrive Code

---
+++ src/main/java/frc/team5332/commands/autonomous/TimeDrive.java
@@ -0,0 +1,21 @@
+package frc.team5332.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team5332.robot.CMap;
+
+public class TimeDrive extends Command {
+    double time, leftspeed, rightspeed;
+
+    public TimeDrive(double time, double leftspeed, double rightspeed) {
+        this.time = time;
+        this.leftspeed = leftspeed;
+        this.rightspeed = rightspeed;
+
+
+    }
+
+    @Override
+    protected void execute() {
+        CMap.drivetrain.setTankDrive( leftspeed, rightspeed);
+    }
+}
